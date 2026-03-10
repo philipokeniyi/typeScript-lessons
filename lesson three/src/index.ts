cd3ec7df7d1978e0728eb5e3bf1b28e658d9e7a9
Philip Okeniyi <okeniyiphilip@gmail.com>
2026-03-10 23:15:49 +0100

    first commit

--- lesson three/src/index.ts
+++ lesson three/src/index.ts
@@ -1,13 +1,49 @@
 // console.log("Hello, TypeScript!");
 
-class User {
-  email: string;
-  name: string;
+// class User {
+//   email: string;
+//   name: string;
+//   readonly city: string = "New York";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+// const philip = new User("philip@example.com", "Philip");
+
+class User {
+ protected _courseCount = 1;
+
+  readonly city: string = "New York";
+  constructor(
+    public email: string,
+    public name: string,
+  ) {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
   }
 }
 
-const philip = new User("philip@example.com", "Philip");
+class SubUser extends User {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 4;
+  }
+}
