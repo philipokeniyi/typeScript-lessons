5de008c474e37285012c7ac6faa7d81bac327bab
Philip Okeniyi <okeniyiphilip@gmail.com>
2026-03-11 10:26:15 +0100

    generic

--- lesson three/src/generic.ts
+++ lesson three/src/generic.ts
@@ -32,3 +32,34 @@ const getMoreSearchProducts = <T>(products: T[]): T => {
   const myIndex = 4;
   return products[myIndex];
 };
+
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunction<T, U>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = [];
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
