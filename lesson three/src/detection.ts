9baffb5f709f6df9d3a735dd97d6ccb46a02b693
Philip Okeniyi <okeniyiphilip@gmail.com>
2026-03-11 11:42:34 +0100

    detection

---
+++ lesson three/src/detection.ts
@@ -0,0 +1,101 @@
+function detectType(val: number | string) {
+  if (typeof val === "string") {
+    return val.toUpperCase();
+  }
+  return val + 3;
+}
+
+function provideId(id: string | null) {
+  if (id) {
+    return id.toUpperCase();
+  }
+  return "No ID provided";
+}
+
+function printAll(strs: string | string[] | null) {
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else {
+      console.log(strs);
+    }
+  }
+}
+
+// narrowing with the in operator
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+}
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+// narrowing with type predicates
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
+
+// discriminated unions
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  sideLength: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.sideLength ** 2;
+    case "rectangle":
+      return shape.length * shape.width;
+  }
+}
+
